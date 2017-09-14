c97ec2c7b4d97f433814db1566f677a6c01d5719
MatrixSeven <hacker.kill07@gmail.com>
2017-09-14 11:44:04 +0800

    更新

--- elpa/monroe-20170623.103/monroe-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "monroe" "20170623.103" "Yet another client for nREPL" 'nil :commit "2bb59ac84e030f7047e7443e2df25185b397a5d3" :url "http://www.github.com/sanel/monroe" :keywords '("languages" "clojure" "nrepl" "lisp"))
