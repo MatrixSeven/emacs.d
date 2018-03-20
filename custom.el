a0454de1ffd670197677aedf694d16afeb5176d0
Seven <hacker.kil07@gmail.com>
2018-03-20 10:51:06 +0800

    更新

---
+++ custom.el
@@ -0,0 +1,17 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (solarizped-theme zenburn-theme use-package solarized-theme rainbow-delimiters powerline org-bullets neotree monroe magit highlight-parentheses exec-path-from-shell elpy diminish diff-hl counsel-projectile clj-refactor challenger-deep-theme atom-one-dark-theme aggressive-indent ag ace-window)))
+ '(tramp-syntax (quote default) nil (tramp)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:foreground "green" :weight normal :height 4.5))))
+ '(mode-line ((t (:foreground "#030303" :background "#bdbdbd" :box nil))))
+ '(mode-line-inactive ((t (:foreground "#f9f9f9" :background "#666666" :box nil)))))
